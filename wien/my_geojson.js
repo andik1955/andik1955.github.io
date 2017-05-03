5c32c7d478b3126caf7485369c1105cf7a76caa8
Schorer <csas2435@uibk.ac.at>
2017-05-03 17:32:18 +0200

    Signed-off-by: Schorer <csas2435@uibk.ac.at>

--- wien/my_geojson.js
+++ wien/my_geojson.js
@@ -1,9 +1,34 @@
 window.my_geojson = {
-	"type": "Point",
-	"coordinates": [16.316, 48.227]
+	"type": "FeatureCollection",
+	"features": [
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [16.316, 48.227]
+		},
+		"properties": {
+			"color": "black"
+		}
+		
+		
+		
+	},
 	
-
-
-
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [16.2, 48.3]
+		},
+		"properties": {
+			"color": "green"
+		}
+		
+		
+		
+	}
 	
+	
+	]
 };
