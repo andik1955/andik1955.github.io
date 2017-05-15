bafd5aad22e7e3f570fc9014ef2c0f91189151d6
Andreas Kollert <Andreas Kollert>
2017-05-15 21:18:06 +0200

    cleaned up html files

---
+++ wien/routing_json.js
@@ -0,0 +1,26 @@
+window.myrouting = {
+    "type": "FeatureCollection",
+    "features": [{
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [47.268583, 11.393264]
+            },
+            "properties": {
+                "color": "black",
+                "name": "Goldenes Dachl"
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [48.208493, 16.373118]
+            },
+            "properties": {
+                "color": "green",
+                "name": "Stephansdom"
+            }
+        }
+    ]
+};
